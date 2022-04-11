fbd337ddc27e64db9048dd96976267b950cccdf7
Joao Carlos Borges Chagas <joao.borges.chagas@avanade.com>
2022-04-11 16:20:49 -0300

    criação do template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
